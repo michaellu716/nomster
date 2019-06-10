187b3bca9111f0be7cd4b830feeb4f7e52fb26a8
Michael Lu <michael_108@hotmail.com>
2019-06-10 23:36:46 +0000

    updated design for nomster 39 for fine tuning

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,7 +4,7 @@ class PlacesController < ApplicationController
 	def index
 		# @places = Place.all
 		# @places = Place.paginate(page: params[:page])
-		@places = Place.page(params[:page]).per_page(5)
+		@places = Place.page(params[:page]).per_page(10)
 	end
 
 	def new
