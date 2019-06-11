cfb6a318540b5e386117ff00b3cd959aec2b71a2
Michael Lu <michael_108@hotmail.com>
2019-06-11 13:59:46 +0000

    nomster 43 send email on comment successfully completed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ApplicationRecord
 	belongs_to :user
 	belongs_to :place
-
+  after_create :send_comment_email
+  
   validates :message, presence: true
   validates :rating, presence: true
 
@@ -16,4 +17,9 @@ class Comment < ApplicationRecord
   def humanized_rating
     RATINGS.invert[self.rating]
   end
+
+  def send_comment_email
+      NotificationMailer.comment_added(self).deliver_now
+  end
+
 end
