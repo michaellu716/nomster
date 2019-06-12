57ccdd7443665675751606737cd007807ee394d4
Michael Lu <michael_108@hotmail.com>
2019-06-12 18:29:50 +0000

    updated some designs after finishing nomster and updated sorting for places in abc order

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,7 +4,8 @@ class PlacesController < ApplicationController
 	def index
 		# @places = Place.all
 		# @places = Place.paginate(page: params[:page])
-		@places = Place.page(params[:page]).per_page(10)
+		@places = Place.order("name").page(params[:page]).per_page(10)
+		@placesLast = Photo.last
 	end
 
 	def new
