3714803dbebf518bbd6ff8a8d8099e051d12e6ec
Michael Lu <michael_108@hotmail.com>
2019-06-13 22:33:12 +0000

    Finished nomster, adding UI updates again

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,7 +4,7 @@ class PlacesController < ApplicationController
 	def index
 		# @places = Place.all
 		# @places = Place.paginate(page: params[:page])
-		@places = Place.order("name").page(params[:page]).per_page(10)
+		@places = Place.order("name").page(params[:page]).per_page(8)
 		@placesLast = Photo.last
 		#place_path(place)
 	end
